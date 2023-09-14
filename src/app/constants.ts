9cef0b860978b6e17875fc051851b8236b711d25
DilipSingh <dilip@Dilip-MBA.fritz.box>
2023-09-14 23:32:24 +0200

    added types and constants file and pagination and http requests for fetching and showing images

---
+++ src/app/constants.ts
@@ -0,0 +1,2 @@
+export const BASE_URL =
+  'http://Server-ts-uploader-env.eba-w8m3mctk.eu-central-1.elasticbeanstalk.com';
--- src/app/paginated-images/paginated-images.component.ts
+++ src/app/paginated-images/paginated-images.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
 
+import { BASE_URL } from '../constants';
+import { ImageItem } from '../types';
+
 @Component({
   selector: 'app-paginated-images',
   templateUrl: './paginated-images.component.html',
 })
 export class PaginatedImagesComponent {
-  url = "https://picture-upload-lytho-dilip.s3.amazonaws.com/pexels-pixabay-220201.jpg"
-  imageList = [this.url,this.url,this.url,this.url,this.url,this.url,this.url,this.url,this.url]
+  imageList: ImageItem[] = [];
+  currentPage: number = 0;
+  lastPage: boolean = false;
+
+  constructor() {
+    this.fetchImages();
+  }
+
+  fetchImages(page: number = 0) {
+    fetch(`${BASE_URL}/images?page=${this.currentPage}`)
+      .then((response) => response.json())
+      .then((data) => {
+        if (!data || !data.length) {
+          this.lastPage = true;
+        }
+        this.imageList = [...data];
+      })
+      .catch((error) => console.error(error));
+  }
+
+  handleBackClick() {
+    this.currentPage = this.currentPage - 1;
+    this.fetchImages(this.currentPage);
+    this.lastPage = false;
+  }
+
+  handleNextClick() {
+    this.currentPage = this.currentPage + 1;
+    this.fetchImages(this.currentPage);
+  }
+
+  handleImageClick(image: ImageItem) {
+    console.log('image clicked!: ', image);
+  }
 }
